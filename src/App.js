3dada19f3cfc08c4bb8517248b5eff7dbc5ee0bc
Shime <152897503+ShimelisTechane@users.noreply.github.com>
2024-05-22 14:26:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+//import Hello from './components/func_compionents';
+   // if we use arrow function we impoert as follow
+import {Hello} from './components/func_compionents';
+import Hi from './components/class_componenets';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className='App'>
+  {/* <Hello></Hello>  also posible*/}
+<Hello />
+<Hi />
+</div>
   );
 }
 
---
+++ src/components/func_compionents.js
@@ -0,0 +1,9 @@
+// import react from 'react';
+// function Hello(){
+//     return <h1>functional component</h1>
+// }
+// export default Hello;
+
+  // when we use arrow function
+  // NB if we use expoert dirrectly before function name we must import {} in app.js
+export const Hello = () =><h1>functional component</h1>
