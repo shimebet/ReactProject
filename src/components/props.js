e21744b846dfc713726907aa9e7d95e1de2035af
Shime <152897503+ShimelisTechane@users.noreply.github.com>
2024-05-22 19:13:15 +0300

    classprops

--- src/components/props.js
+++ src/components/props.js
@@ -1,13 +1,24 @@
-import React from 'react'
+// import React from 'react'
 
-const FullName = (props) => {
-    return (
-        <div>
-  <h1>{props.name} Kebede  {props.Age}</h1> 
-{props.children}
-        </div>
+// const FullName = (props) => {
+//     return (
+//         <div>
+//   <h1>{props.name} Kebede  {props.Age}</h1> 
+// {props.children}
+//         </div>
       
-);
+// );
+// }
+// export default FullName;
+import React, { Component } from 'react'
+
+export default class props extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.name} Kebede  {this.props.Age}</h1>
+        {this.props.children}
+      </div>
+    )
+  }
 }
- 
-export default FullName;
